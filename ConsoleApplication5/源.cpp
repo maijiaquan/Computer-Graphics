b462b43debb3efcde0a3cfef46c97294619534c8
xiaomingmai <499466765@qq.com>
2017-08-12 11:55:46 +0800

    11.55

--- ConsoleApplication5/源.cpp
+++ ConsoleApplication5/源.cpp
@@ -5,6 +5,11 @@ int main(){
 	std::cout<<"hi"<<std::endl;
 	std::cout<<"hi2"<<std::endl;
 
+	for(int i = 0; i < 10; i ++){
+		if(i < 10){
+			std::cout<<"hi"<<std::endl;
+		}
+	}
 	system("pause");
 	return 0;
 
